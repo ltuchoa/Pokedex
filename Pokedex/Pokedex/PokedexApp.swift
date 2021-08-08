306f43b88b19ab222485e47352fc276119ea8d6a
Larissa <ltuchoa@gmail.com>
2021-08-08 16:08:07 -0300

    created project

---
+++ Pokedex/Pokedex/PokedexApp.swift
@@ -0,0 +1,17 @@
+//
+//  PokedexApp.swift
+//  Pokedex
+//
+//  Created by Larissa Uchoa on 08/08/21.
+//
+
+import SwiftUI
+
+@main
+struct PokedexApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
